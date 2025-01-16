507d94cc71289a57ddc369c1d6a25b104d3f11f4
Sunro1994 <gwangjulsr@gmail.com>
2025-01-16 14:01:42 +0900

    Security 의존성 추가 및 패키지 이름 수정, Bean 수동 등록 테스트

--- src/main/java/com/sparta/springauth/SpringAuthApplication.java
+++ src/main/java/com/sparta/springauth/SpringAuthApplication.java
@@ -1,9 +1,10 @@
-package org.sparta.springauth;
+package com.sparta.springauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) //초기 학습을 위해 인증 기능 제외
 public class SpringAuthApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/sparta/springauth/SpringAuthApplicationTests.java
+++ src/test/java/com/sparta/springauth/SpringAuthApplicationTests.java
@@ -1,4 +1,4 @@
-package org.sparta.springauth;
+package com.sparta.springauth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
