351ab566983bae659d9cdcd59ba300f47c79dbd2
Sunro1994 <gwangjulsr@gmail.com>
2025-01-19 14:19:15 +0900

    Spring Security 인가 처리

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -34,13 +34,5 @@ public class UserController {
         userService.signup(requestDto);
         return "redirect:/api/user/login-page";
     }
-    @PostMapping("/user/login")
-    public String login(LoginRequestDto requestDto , HttpServletResponse resp){
-        try {
-            userService.login(requestDto,resp);
-        } catch (Exception e) {
-           return "redirect:/api/user/login-page?error";
-        }
-        return "redirect:/";
-    }
+
 }
