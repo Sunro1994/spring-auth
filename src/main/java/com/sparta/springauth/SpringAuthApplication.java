d771bd74b469837f31108a853ddc6d70f380d11b
Sunro1994 <gwangjulsr@gmail.com>
2025-01-19 13:53:39 +0900

    Spring Security 적용

--- src/main/java/com/sparta/springauth/SpringAuthApplication.java
+++ src/main/java/com/sparta/springauth/SpringAuthApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class) //초기 학습을 위해 인증 기능 제외
+@SpringBootApplication //초기 학습을 위해 인증 기능 제외
 public class SpringAuthApplication {
 
     public static void main(String[] args) {
